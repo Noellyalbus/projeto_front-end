debd3bb0307303e9b907585382e47bc0388f7145
Ednilson-Oli <ednilsonoliveira2@hotmail.com>
2025-06-13 11:08:51 -0300

    Fix class attribute to className in Checkbox component

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,12 +3,12 @@
 import checked from '../../../assets/checkbox-checked.svg';
 function Checkbox({ label, ...props }) {
     return (
-        <label class="relative flex items-center cursor-pointer space-x-2 ">
-            <input type="checkbox" class="sr-only peer" {...props} />
-            <div class="w-5 h-5 bg-white border-1 border-gray-300 rounded-xs">
+        <label className="relative flex items-center cursor-pointer space-x-2 ">
+            <input type="checkbox" className="sr-only peer" {...props}  />
+            <div className="w-5 h-5 bg-white border-1 border-gray-300 rounded-xs">
             </div>
-            <img src={checked} alt="Checked" class="w-5 h-5 absolute opacity-0 peer-checked:opacity-100 transition-opacity" />
-            <span>{label}</span>
+            <img src={checked} alt="Checked" className="w-5 h-5 absolute opacity-0 peer-checked:opacity-100 transition-opacity" />
+            <span className='text-sm text-(--dark-gray-2)'>{label}</span>
         </label>
 
     );
